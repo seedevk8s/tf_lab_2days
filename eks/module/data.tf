683b924e7f76ef504b7859007d04dab73f6b57fb
seedevk8s <seedevk8s@gmail.com>
2024-08-25 16:39:12 +0900

    추가 실습

---
+++ eks/module/data.tf
@@ -0,0 +1,29 @@
+#data "aws_eks_cluster" "cluster" {
+#  name = module.eks.cluster_name
+#}
+
+
+data "aws_caller_identity" "current" {}
+
+
+data "aws_security_group" "default-sg" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
+
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.virginia
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+
+}
---
+++ eks/module/variable.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+}
+variable "cluster_version" {
+}
+variable "vpc_id" {
+}
+variable "public_subnets" {
+}
+variable "private_subnets" {
+}
+variable "region" {
+}
+
+
